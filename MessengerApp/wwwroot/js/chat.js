35acb486d06b9d9938a459fbd28d3995eebfbaba
idbditc@gmail.com <idbditc@gmail.com>
2024-02-05 19:03:08 +0600

    Add project files.

---
+++ MessengerApp/wwwroot/js/chat.js
@@ -0,0 +1,46 @@
+﻿/// <reference path="../lib/microsoft/signalr/dist/browser/signalr.js" />
+
+
+
+
+var username = prompt("enter your name", "DITC");
+
+var hubBuilder = new signalR.HubConnectionBuilder();
+var connection = hubBuilder.withUrl("/chathub").build();
+
+
+
+
+connection.on("msgRcv", function (user, message) {
+    var a = document.createElement("li");
+
+
+    a.textContent = `${user} : ${message}`;
+
+    $('#chatLog').html(a);
+
+    
+});
+connection.on("selfMsg", function ( message) {
+    var a = document.createElement("li");
+
+
+    a.textContent = `(self) : ${message}`;
+
+    //$('#chatLog').append(a);
+    $(a).appendTo($('#chatLog'));
+
+
+});
+
+
+connection.start();
+
+
+$('#btnSend').on('click', function () {
+
+    var message = $('#txtInput').val();
+
+    connection.invoke("Share", username, message);
+    $('#txtInput').val(null);
+})
