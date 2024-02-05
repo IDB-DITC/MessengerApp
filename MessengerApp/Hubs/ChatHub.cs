b8e45da7abfa3fe829561475dee16f9c272ac7d8
idbditc@gmail.com <idbditc@gmail.com>
2024-02-05 19:03:08 +0600

    Add project files.

---
+++ MessengerApp/Hubs/ChatHub.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessengerApp.Hubs
+{
+	public class ChatHub:Hub
+	{
+		public async Task Share(string user, string message)
+		{
+			//await Clients.All.SendAsync("receive", user, message);
+			await Clients.Caller.SendAsync("receive", "-----", message);
+			await Clients.Others.SendAsync("receive", user, message);
+
+		}
+
+
+
+		public override Task OnConnectedAsync()
+		{
+
+
+
+			return base.OnConnectedAsync();
+		}
+
+		public override Task OnDisconnectedAsync(Exception? exception)
+		{
+			return base.OnDisconnectedAsync(exception);
+		}
+	}
+}
